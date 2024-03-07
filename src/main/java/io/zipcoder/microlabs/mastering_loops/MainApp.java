6610f86652a37d04d04c72b459135384c72d4fa5
Szeng89 <shijinz898@gmail.com>
2024-03-07 08:02:30 -0500

    figured needed a nested loop for tables

--- src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
@@ -7,5 +7,13 @@ import java.util.Scanner;
  */
 public class MainApp {
     public static void main(String[] args) {
+
+        for (int i =1; i <= 5; i++){
+            System.out.println(i);
+            for(int j =1; j <= 5; j++){
+                System.out.println(i*j);
+            }
+
+        }
     }
 }
