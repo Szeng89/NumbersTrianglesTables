048e6af309ce0db7f7473864cb557c459d0586ae
Szeng89 <shijinz898@gmail.com>
2024-03-07 11:46:09 -0500

    added Large table method with a nested loop

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,11 +2,41 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= 5; j++){
+
+                int number = i * j;
+                if (number <= 9){
+                    sb.append("  "+number+" ").append("|");
+                } else{
+                    sb.append(" "+number+" ").append("|");
+                }
+            }
+            sb.append("\n");
+
+        }
+        return sb.toString();
     }
 
+
+
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder sb2 = new StringBuilder();
+        for ( int i = 1; i <=10; i++){
+            for (int j = 1; j <= 10; j++){
+                int number = j * i;
+                if (number < 10){
+                    sb2.append("  "+ number +" " + "|");
+                }else if (number <= 99) {
+                    sb2.append(" " + number +" " + "|");
+                }else{
+                    sb2.append(number + " " + "|");
+                }
+            }
+            sb2.append("\n");
+        }
+        return sb2.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
